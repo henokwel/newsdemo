a7a2788a8b14ab2863f5445f3c58f38ac548868f
henWel <henokwel@outlook.com>
2020-10-10 07:02:30 +0200

    Updates

---
+++ precache-manifest.dfd076ae716ec3d5a368b7275b8a0ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855cd50ddb7aa3fab63abe4ad3a0e26a",
+    "url": "/newsdemo/index.html"
+  },
+  {
+    "revision": "49faf73d2519d90419b7",
+    "url": "/newsdemo/static/css/main.cb928399.chunk.css"
+  },
+  {
+    "revision": "7895b4190ac4b56344bc",
+    "url": "/newsdemo/static/js/2.51b927d2.chunk.js"
+  },
+  {
+    "revision": "ba46ae7961bea589e3a4a9ad90bea01e",
+    "url": "/newsdemo/static/js/2.51b927d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49faf73d2519d90419b7",
+    "url": "/newsdemo/static/js/main.fe8a73ac.chunk.js"
+  },
+  {
+    "revision": "233f349f86875784d670",
+    "url": "/newsdemo/static/js/runtime-main.d2e2b8ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newsdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
